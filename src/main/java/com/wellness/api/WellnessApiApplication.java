1f3386e219d6ea920011be047d1611f17e2b7750
diya <devops@in.ibm.com>
2025-06-19 21:55:37 +0530

    Configure Maven and Spring Boot for Wellness API

---
+++ src/main/java/com/wellness/api/WellnessApiApplication.java
@@ -0,0 +1,11 @@
+package com.wellness.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellnessApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WellnessApiApplication.class, args);
+    }
+}
